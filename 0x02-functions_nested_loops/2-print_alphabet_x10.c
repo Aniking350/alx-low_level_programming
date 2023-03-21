09002d3e9f9598e1ca453403e516ecf60394bcc8
Aniking350 <Kingsleytitus20@gmail.com>
2023-03-21 14:53:05 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,20 @@
  * Return: void
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 
 {
-
+	
 	char c;
+	int i = 0;
 
-	for (c = 'a'; c <= 'z'; c++)
+	while (i <= 9)
+	{
+	for (c = 'a'; c <= "z'; c++)
 	{
 	_putchar(c);
 	}
 	_putchar('\n');
+	i++;
+	}
 }
