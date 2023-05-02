5f8f9e4f80b669a2ed82f20230448e66784dd6d4
Aniking350 <Kingsleytitus20@gmail.com>
2023-05-02 08:19:05 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,15 +1,14 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked lists,
+ * insert_nodeint_at_index - inserts a new node in a linked list,
  * at a given position
- * @head: pointer to the first node in the lists
+ * @head: pointer to the first node in the list
  * @idx: index where the new node is added
  * @n: data to insert in the new node
  *
  * Return: pointer to the new node, or NULL
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
@@ -17,22 +16,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp = *head;
 
 	new = malloc(sizeof(listint_t));
-	if(!new || !head)
-		return(NULL);
+	if (!new || !head)
+		return (NULL);
 
 	new->n = n;
 	new->next = NULL;
 
-	if(idx == 0)
+	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
-		return(new);
+		return (new);
 	}
 
-	for(i = 0; temp && i < idx; i++)
+	for (i = 0; temp && i < idx; i++)
 	{
- 		if(i == idx - 1)
+		if (i == idx - 1)
 		{
 			new->next = temp->next;
 			temp->next = new;
